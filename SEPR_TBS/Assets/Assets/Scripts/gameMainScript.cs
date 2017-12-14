507503dc3e61b898aada881fd2df33ac0815829c
BJStevens69 <fbjs500@york.ac.uk>
2017-12-14 01:58:28 +0000

    Battle V2
    Battle now plays a placeholder animation sprite and ends correctly.

--- SEPR_TBS/Assets/Assets/Scripts/gameMainScript.cs
+++ SEPR_TBS/Assets/Assets/Scripts/gameMainScript.cs
@@ -9,7 +9,8 @@ public class gameMainScript : MonoBehaviour {
     public List<player> playerList = new List<player>(); //list of players, details of which should be passed fom the main menu
     public Canvas battleDisplay;                        // Canvas used to display battles
     public GameObject gameMap;
-    private float counter;
+    Animator battleAnimatorA;
+    Animator battleAnimatorB;
 
 
     // Use this for initialization
@@ -20,7 +21,7 @@ public class gameMainScript : MonoBehaviour {
         playerList.Add(new player("jamie", Color.blue, 50));
     }
 
-    public void Awake()
+    public void Start()
     {
         GameObject temp = GameObject.Find("DisplayBattle");  // Finding the Object holding the canvas
         if (temp != null)
@@ -34,10 +35,10 @@ public class gameMainScript : MonoBehaviour {
     {
         unitScript unitAScript = unitA.GetComponent<unitScript>();
         unitScript unitBScript = unitB.GetComponent<unitScript>();
-        int damage = unitAScript.GetAttack();
+        int damage = unitAScript.GetAttack();  // Placeholder calculation, will be replace by more sophisticated method
         if (unitAScript.GetAttack() < unitBScript.GetCurHP())
         {
-            unitBScript.Damage(damage); // Placeholder calculation, will be replace by more sophisticated method
+            unitBScript.Damage(damage);
             DisplayBattle(unitA, unitB, damage, false);
         }
         else
@@ -50,16 +51,34 @@ public class gameMainScript : MonoBehaviour {
 
     public void DisplayBattle(GameObject unitA, GameObject unitB, int damage, bool isDead) // unitA is aggressor, unitB is defender. Will bring up background map and correct units, display attack animation and correct damage number.
     {  
-        battleDisplay.enabled = true;                                                       // Display the canvas
-        Image[] sprites = battleDisplay.GetComponentsInChildren<Image>();                   // Setup the appropriate sprites for the units involved.
-        sprites[1].sprite = unitA.GetComponent<SpriteRenderer>().sprite;
-        sprites[2].sprite = unitB.GetComponent<SpriteRenderer>().sprite;
-        sprites[2].transform.localRotation = Quaternion.Euler(0, 180, 0);
-        // TODO
-        // Play attack animation and damage number
-        // Wait appropriate time
-        // Disable display
+        battleDisplay.enabled = true;       // Display the canvas
+        GameObject temp1 = GameObject.Find("unitA");
+        battleAnimatorA = temp1.GetComponent<Animator>();
+        GameObject temp2 = GameObject.Find("unitB");
+        battleAnimatorB = temp2.GetComponent<Animator>();
+        temp1.GetComponent<Image>().sprite = unitA.GetComponent<SpriteRenderer>().sprite;
+        temp2.GetComponent<Image>().sprite = unitB.GetComponent<SpriteRenderer>().sprite; // setup correct sprites
+        temp2.GetComponent<Image>().transform.localRotation = Quaternion.Euler(0, 180, 0);
+        battleAnimatorA.SetBool("Battle", true);                                          // Play the battle animation for both units (at the moment only attacking unit)
+        Invoke ("Animating", 1);                                                          // Timer to stop animation playing twice
+        
 
+        // TODO
+        // Display damage number
+        // Alter animation for unitB if isDead is true.
+    }
+
+    private void Animating()                                                              // This function stops the attack animation playing more than once, also times the end of the battle.
+    {
+        battleAnimatorA.SetBool("AnimationPlaying", true);
+        Invoke("EndDisplayBattle", 1);
+    }
+
+    private void EndDisplayBattle()                                                       // This function resets any variables used and causes the battle canvas to be hidden again.
+    {
+        battleAnimatorA.SetBool("Battle", false);
+        battleAnimatorA.SetBool("AnimationPlaying", false);
+        battleDisplay.enabled = false;
     }
 	
 	// Update is called once per frame
